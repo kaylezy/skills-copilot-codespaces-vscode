5568670de021b36ec24a0598533d4953d097f8f6
Olakunle Hassan <kunlex_042003@yahoo.com>
2023-11-25 12:35:31 +0000

    Copilot thir commit

--- comment.js
+++ comment.js
@@ -19,3 +19,102 @@ router.put('/:commentID', commentController.updateComment);
 router.delete('/:commentID', commentController.deleteComment);
 
 module.exports = router;
+
+//comment web server2
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+//Get all comments
+router.get('/', commentController.getAllComments);
+
+//Get comments by postID
+router.get('/post/:postID', commentController.getCommentsByPostID);
+
+//Create new comment
+router.post('/', commentController.createComment);
+
+//Update comment
+router.put('/:commentID', commentController.updateComment);
+
+//Delete comment
+router.delete('/:commentID', commentController.deleteComment);
+
+module.exports = router;
+
+//comment web server3
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+//Get all comments
+router.get('/', commentController.getAllComments);
+
+//Get comments by postID
+router.get('/post/:postID', commentController.getCommentsByPostID);
+
+//Create new comment
+router.post('/', commentController.createComment);
+
+//Update comment
+router.put('/:commentID', commentController.updateComment);
+
+//Delete comment
+router.delete('/:commentID', commentController.deleteComment);
+
+module.exports = router;
+
+//comment web server4
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+//Get all comments
+router.get('/', commentController.getAllComments);
+
+//Get comments by postID
+router.get('/post/:postID', commentController.getCommentsByPostID);
+
+//Create new comment
+router.post('/', commentController.createComment);
+
+//Update comment
+router.put('/:commentID', commentController.updateComment);
+
+//Delete comment
+router.delete('/:commentID', commentController.deleteComment);
+
+module.exports = router;
+
+//comment web server5
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+//Get all comments
+router.get('/', commentController.getAllComments);
+
+//Get comments by postID
+router.get('/post/:postID', commentController.getCommentsByPostID);
+
+//Create new comment
+router.post('/', commentController.createComment);
+
+//Update comment
+router.put('/:commentID', commentController.updateComment);
+
+//Delete comment
+router.delete('/:commentID', commentController.deleteComment);
+
+module.exports = router;
+
+//comment web server6
+const express = require('express');
+const router = express.Router();
+const commentController = require('../controllers/commentController');
+
+//Get all comments
+router.get('/', commentController.getAllComments);
+
+//Get comments by postID
+router.get('/post
